6d82e2357afd90acce54784d6a66ad28ce7e3c7f
David Schaefer <david.schaefe@gmail.com>
2016-01-18 23:17:46 +0100

    a very basic main file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 
+#include <vector>
+#include <numeric>
+#include <iostream>
 #include "ndarray.hpp"
 
-int main(int argc, const char* argv[]){
+template<typename T>
+void printVector(std::vector<T> v){
+	for (auto value : v){
+		std::cout << value << " ";
+	}
+	std::cout << std::endl;
+}
+
+std::vector<int> range(int start, int stop){
+	if (stop <= start){
+		// should be something nicer...
+		throw 10;
+	}
+	std::vector<int> out(stop-start);
+	std::iota(std::begin(out), std::end(out), start);
+	return out;
+}
+
+int main(){
+
+	std::vector<size_t> shape = {16,16};
+	std::vector<int> base = range(0,16*16);
 	
+	Ndarray<int>array(&base[0],shape,1);
+
+	return 0;
 }
